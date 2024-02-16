8857987b3817592265400b0a508c4ba005d27fef
Caiquedevs <caiquedevs@gmail.com>
2024-02-16 20:47:22 -0300

    wip

--- App.tsx
+++ App.tsx
@@ -1,78 +1,69 @@
-import React, { useRef } from "react";
-import { StatusBar } from "expo-status-bar";
-import Constants from "expo-constants";
+import React from 'react';
+import { Text, TextInput, View, Dimensions } from 'react-native';
+import Constants from 'expo-constants';
 
-import {
-  StyleSheet,
-  View,
-  Alert,
-  BackHandler,
-} from "react-native";
-import { WebView } from "react-native-webview";
+import Carousel from 'react-native-snap-carousel';
 
-export default function App() {
-  const webViewRef = useRef<any>(null);
-  const hasNavigationHistory = useRef(false);
+type Props = {};
 
-  React.useEffect(() => {
-    const backAction = () => {
-      if (
-        webViewRef.current &&
-        hasNavigationHistory.current
-      ) {
-        webViewRef.current.goBack();
-        return true;
-      } else {
-        Alert.alert(
-          "Aviso",
-          "Deseja sair do aplicativo?",
-          [
-            {
-              text: "Cancelar",
-              onPress: () => null,
-              style: "cancel",
-            },
-            {
-              text: "Sim",
-              onPress: () =>
-                BackHandler.exitApp(),
-            },
-          ]
-        );
-        return true;
-      }
-    };
+export default function App({}: Props) {
+  const windowWidth = Dimensions.get('window').width;
 
-    const backHandler =
-      BackHandler.addEventListener(
-        "hardwareBackPress",
-        backAction
-      );
-
-    return () => backHandler.remove();
-  }, []);
+  const [items, setItems] = React.useState({
+    activeIndex: 0,
+    carouselItems: [
+      {
+        title: 'Item 1',
+        text: 'Text 1',
+      },
+      {
+        title: 'Item 2',
+        text: 'Text 2',
+      },
+      {
+        title: 'Item 3',
+        text: 'Text 3',
+      },
+      {
+        title: 'Item 4',
+        text: 'Text 4',
+      },
+      {
+        title: 'Item 5',
+        text: 'Text 5',
+      },
+    ],
+  });
 
   return (
-    <View style={styles.container}>
-      <StatusBar style="light" />
-      <WebView
-        ref={webViewRef}
-        onNavigationStateChange={(navState) => {
-          hasNavigationHistory.current =
-            navState.canGoBack;
-        }}
-        source={{
-          uri: "http://192.168.1.10:3000/?render=render-stock-list-now-6473c006849ea6826f57ff12",
-        }}
-      />
+    <View style={{ paddingTop: Constants.statusBarHeight }} className="w-full flex-1 bg-slate-900">
+      <View className="bg-blue-500">
+        <Carousel
+          layout={'default'}
+          vertical={false}
+          data={items.carouselItems}
+          sliderWidth={windowWidth}
+          itemWidth={300}
+          onSnapToItem={(index) => setItems((old) => ({ ...old, activeIndex: index }))}
+          renderItem={({ item, index }) => {
+            return (
+              <View
+                style={{
+                  backgroundColor: 'red',
+                  borderRadius: 5,
+                  height: 250,
+                  padding: 50,
+                  marginLeft: 0,
+                  marginRight: 0,
+                }}
+              >
+                <Text style={{ fontSize: 30 }}>{item.title}</Text>
+                <Text>{item.text}</Text>
+              </View>
+            );
+          }}
+        />
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: "#24262a",
-  },
-});
